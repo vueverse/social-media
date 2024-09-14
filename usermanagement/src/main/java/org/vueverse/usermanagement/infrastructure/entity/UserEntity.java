32e2c0492d0fe28b4259de10f61c98a0ca42eb05
ali <ali77erfagh99@gmail.com>
2024-09-14 16:01:12 +0330

    add base user , and property for UserEntity

--- usermanagement/src/main/java/org/vueverse/usermanagement/infrastructure/entity/UserEntity.java
+++ usermanagement/src/main/java/org/vueverse/usermanagement/infrastructure/entity/UserEntity.java
@@ -1,4 +1,25 @@
 package org.vueverse.usermanagement.infrastructure.entity;
 
-public class UserEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.vueverse.usermanagement.infrastructure.entity.base.BaseEntity;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity<Long> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_generator")
+    @SequenceGenerator(name = "user_generator", sequenceName = "user_seq", allocationSize = 1)
+    private Long id;
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
 }
---
+++ usermanagement/src/main/java/org/vueverse/usermanagement/infrastructure/entity/base/BaseEntity.java
@@ -0,0 +1,29 @@
+package org.vueverse.usermanagement.infrastructure.entity.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@MappedSuperclass
+public abstract class BaseEntity<ID> {
+
+    @Builder.Default
+    @Column(name = "created_date", columnDefinition = "timestamp", nullable = false)
+    private LocalDateTime createdDate = LocalDateTime.now();
+
+    @Column(name = "updated_date", columnDefinition = "timestamp", nullable = false)
+    private LocalDateTime updatedDate;
+    @Column(name = "deleted", columnDefinition = "timestamp")
+    private LocalDateTime deleted;
+
+    public abstract ID getId();
+
+    public abstract void setId(ID id);
+}
