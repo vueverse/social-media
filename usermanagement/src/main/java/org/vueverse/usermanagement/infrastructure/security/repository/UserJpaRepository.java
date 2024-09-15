69cbd84dfab2a77cd5d376c86ee17f9c04c9219a
ali <ali77erfagh99@gmail.com>
2024-09-15 15:41:59 +0330

    add find by username

--- usermanagement/src/main/java/org/vueverse/usermanagement/infrastructure/security/repository/UserJpaRepository.java
+++ usermanagement/src/main/java/org/vueverse/usermanagement/infrastructure/security/repository/UserJpaRepository.java
@@ -1,10 +1,14 @@
 package org.vueverse.usermanagement.infrastructure.security.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import org.vueverse.usermanagement.infrastructure.security.entity.UserEntity;
 
 import java.util.Optional;
 
+@Repository
 public interface UserJpaRepository extends CrudRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
+
+    Optional<UserEntity> findByUsername(String username);
 }
