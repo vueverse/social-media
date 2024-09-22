b919557787a13a1e7e5ed5af87354c9e3f5de395
aliJavaTar <ali77erfagh99@gmail.com>
2024-09-22 16:50:31 +0330

    add docker

--- usermanagement/src/main/java/org/vueverse/usermanagement/UsermanagementApplication.java
+++ usermanagement/src/main/java/org/vueverse/usermanagement/UsermanagementApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class UsermanagementApplication {
 
     public static void main(String[] args) {
