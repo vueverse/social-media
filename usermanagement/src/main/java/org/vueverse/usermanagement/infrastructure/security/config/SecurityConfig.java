32d23c56f569f8395a34cf3c9cfc2e8eecbe86d1
aliJavaTar <ali77erfagh99@gmail.com>
2024-09-23 14:14:58 +0330

    add config

--- usermanagement/src/main/java/org/vueverse/usermanagement/infrastructure/security/config/SecurityConfig.java
+++ usermanagement/src/main/java/org/vueverse/usermanagement/infrastructure/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.vueverse.usermanagement.infrastructure.security.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +9,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.vueverse.usermanagement.infrastructure.security.filter.JWTTokenGeneratorFilter;
@@ -18,7 +21,8 @@ import org.vueverse.usermanagement.infrastructure.security.filter.JWTTokenValida
 public class SecurityConfig {
     private final JWTTokenGeneratorFilter jwtTokenGeneratorFilter;
     private final JWTTokenValidatorFilter jwtTokenValidatorFilter;
-
+    @Value("&{spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -41,5 +45,9 @@ public class SecurityConfig {
         return config.getAuthenticationManager();
     }
 
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+    }
 
 }
